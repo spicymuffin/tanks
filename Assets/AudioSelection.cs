74be4e9d55597d0d1501cd2ae3d41db6bff0cc17
Quizert <alen.123@bk.ru>
2021-03-29 17:09:19 +0300

    Исправил баг
    было весело

---
+++ Assets/AudioSelection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class AudioSelection : MonoBehaviour
+{
+    public AudioSource hittingWallSound1;
+    public AudioSource hittingWallSound2;
+    public System.Random rand = new System.Random();
+
+    public void TheChoice()
+    {
+        int value = rand.Next(1, 3);
+        if(value == 1)
+        {
+            Debug.Log('1');
+            hittingWallSound1.Play();
+        }
+        else
+        {
+            Debug.Log('2');
+            hittingWallSound2.Play();
+        }
+    }
+
+
+    public void Awake()
+    {
+        TheChoice();
+        StartCoroutine(Destroyer());
+    }
+
+
+    IEnumerator Destroyer()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,17 +6,16 @@ public class Rocket : MonoBehaviour
 {
     public float speed;
     public GameObject HitEffect;
+    public GameObject audioPlayer;
     public SelfDestruct smokeTrail;
-    public AudioSource hittingTheWallSound1;
-    public AudioSource hittingTheWallSound2;
-    public Renderer rend;
-    public System.Random rand = new System.Random();
     public Player sender;
+
 
     private void FixedUpdate()
     {
         transform.position += transform.forward * speed;
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -37,15 +36,7 @@ public class Rocket : MonoBehaviour
         }
         if (!other.CompareTag("CloseCallBox"))
         {
-            int value = rand.Next(1, 3);
-            if (value == 1)
-            {
-                hittingTheWallSound1.Play();
-            }
-            else
-            {
-                hittingTheWallSound2.Play();
-            }
+            GameObject AudioPlayer = Instantiate(audioPlayer);
             if (other.CompareTag("Shield"))
             {
                 if (other.GetComponent<ShieldScript>().player == sender)
@@ -56,9 +47,10 @@ public class Rocket : MonoBehaviour
             GameObject instance = Instantiate(HitEffect, transform.position, Quaternion.identity);
             smokeTrail.gameObject.transform.parent = GameObject.FindGameObjectWithTag("Effects").transform;
             smokeTrail.Destroy();
-            StartCoroutine(Wait());
+            Destroy(this.gameObject);
         }
     }
+
 
     private void Awake()
     {
@@ -66,17 +58,10 @@ public class Rocket : MonoBehaviour
         StartCoroutine(Destroy());
     }
 
+
     IEnumerator Destroy()
     {
         yield return new WaitForSeconds(20);
-        Destroy(this.gameObject);
-    }
-
-    IEnumerator Wait()
-    {
-
-        rend.enabled = false;
-        yield return new WaitForSeconds(1f);
         Destroy(this.gameObject);
     }
 }
