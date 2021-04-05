1cba028f7a719e2793c62cb00d84623500f8a47a
Quizert <alen.123@bk.ru>
2021-04-05 12:50:03 +0300

    Если вдруг меня не станет, бэйби (бэйби)

--- Assets/Scripts/RicRocket.cs
+++ Assets/Scripts/RicRocket.cs
@@ -6,7 +6,8 @@ public class RicRocket : MonoBehaviour
 {
     public float speed;
     public Player sender;
-    public int maxDeflects = 2;
+    public int maxDeflects = 3;
+    public SelfDestruct smokeTrail;
 
     private int deflectCounter = 0;
     private void FixedUpdate()
@@ -25,26 +26,33 @@ public class RicRocket : MonoBehaviour
                 Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
                 float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
                 transform.eulerAngles = new Vector3(0, rot, 0);
+
             }
             else
             {
+                smokeTrail.gameObject.transform.parent = LevelConfig.instance.effects;
+                smokeTrail.Destroy();
                 Destroy(this.gameObject);
             }
-            if (hit.collider.transform.parent.CompareTag("Player"))
+            Transform parent = hit.collider.transform.parent;
+            if (parent)
             {
-                Player hitPlayer;
-                hitPlayer = hit.collider.GetComponent<Player>();
-                if (hitPlayer == sender)
+                if (hit.collider.transform.parent.CompareTag("Player"))
                 {
-                    hitPlayer.BulletDie();
-                    Debug.Log($"killed: {hitPlayer.username}");
-                    sender.kills--;
-                }
-                else
-                {
-                    hitPlayer.BulletDie();
-                    Debug.Log($"killed: {hitPlayer.username}");
-                    sender.kills++;
+                    Player hitPlayer;
+                    hitPlayer = hit.collider.GetComponent<Player>();
+                    if (hitPlayer == sender)
+                    {
+                        hitPlayer.BulletDie();
+                        Debug.Log($"killed: {hitPlayer.username}");
+                        sender.kills--;
+                    }
+                    else
+                    {
+                        hitPlayer.BulletDie();
+                        Debug.Log($"killed: {hitPlayer.username}");
+                        sender.kills++;
+                    }
                 }
             }
         }
