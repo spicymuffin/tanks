f6e99da3e27407cf9dcf64f462351e612d4e3dc0
Quizert <alen.123@bk.ru>
2021-04-08 22:33:48 +0300

    Эй, если вдруг меня не станет, брат (брат)
    a

--- Assets/Scripts/LightMovement.cs
+++ Assets/Scripts/LightMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LightMovement : MonoBehaviour
 {
     public new Transform transform;
-    public int rotationAngle;
+    public float rotationAngle;
     public void FixedUpdate()
     {
         transform.Rotate(new Vector3(rotationAngle, 0, 0) * Time.deltaTime);
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,16 +7,22 @@ public class SoundManager : MonoBehaviour
 {
     public Slider slider;
     public static float soundFloat = 0.5f;
+    public AudioSource sound;
 
 
     public void Start()
     {
         slider.value = PlayerPrefs.GetFloat("save", soundFloat);
+        sound.mute = true;
     }
+
 
     public void SaveSoundSettings(float value)
     {
         soundFloat = value;
-        PlayerPrefs.SetFloat("save", soundFloat); 
+        PlayerPrefs.SetFloat("save", soundFloat);
+        sound.volume = soundFloat;
+        sound.mute = false;
+        sound.Play();
     }
 }
