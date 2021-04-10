c7a350cdec7c39e6c0e8a4079b84ec4296fc599d
Quizert <alen.123@bk.ru>
2021-04-10 15:25:50 +0300

    Я был первым для тебя, но не парадов (я)

--- Assets/PlatformMovement.cs
+++ Assets/PlatformMovement.cs
@@ -4,24 +4,41 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    public float speed;
+    public float startMoveDuration;
+    public float endMoveDuration;
+    public GameObject target;
 
     public void Start()
     {
-        Wait();
-    }
-
-    public void FixedUpdate()
-    {
-        transform.Translate(speed, 0, 0);
-        
+        Transform lol = target.GetComponent<Transform>();
+        StartCoroutine(MoveCoroutine(lol));
     }
 
 
-    IEnumerator Wait()
+    IEnumerator MoveCoroutine(Transform target)
     {
-        yield return new WaitForSeconds(1f);
-        speed = -speed;
-        StartCoroutine(Wait());
+        float t = 0.0f;
+        float time = 0.0f;
+        Vector3 start = transform.position;
+        Vector3 end = target.position;
+
+        while(true)
+        {
+            while (t < startMoveDuration)
+            {
+                t += Time.deltaTime;
+                transform.position = Vector3.Lerp(start, end, t / startMoveDuration);
+                yield return null;
+            }
+            while (time < endMoveDuration)
+            {
+                time += Time.deltaTime;
+                transform.position = Vector3.Lerp(end, start, time / endMoveDuration);
+                yield return null;
+            }
+            t = 0.0f;
+            time = 0.0f;
+        }
+
     }
 }
