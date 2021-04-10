33e64007a7155a07dc5f794285e2e4d80f1da680
Quizert <alen.123@bk.ru>
2021-04-10 14:52:40 +0300

    Apenado, но меня не будет рядом

---
+++ Assets/PlatformMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public float speed;
+
+    public void Start()
+    {
+        Wait();
+    }
+
+    public void FixedUpdate()
+    {
+        transform.Translate(speed, 0, 0);
+        
+    }
+
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1f);
+        speed = -speed;
+        StartCoroutine(Wait());
+    }
+}
